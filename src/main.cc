9659fb07208f2a94033a82670a75d8ecc373d0f3
jeakyungc <jeakyungc@gmail.com>
2025-01-17 17:52:02 +0900

    5. Adding a Sphere - Ray-Sphere Intersection - Creating Our First Raytraced Image - Error Fixed in calculating viewport_width

--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,25 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = center - r.origin();  // C - Q
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0f * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+
+    auto discriminat = b*b - 4*a*c;
+    
+    return (discriminat >= 0); // if >=0, there is 1 or 2 real value solution.
+}
+
 color ray_color(const ray& r)
 {
+    // Place sphere centered at (0,0,-1) with radius 0.5f,
+    // thus, intersection of viewport plane and sphere makes circle paralle to xy plane.
+    // Return red color if ray intersects with ray from camera eye.
+    if (hit_sphere(point3(0,0,-1), 0.5f, r)) return color(1,0,0); 
+
     // linear interpolation (lerp) of white to skyblue color along the y height
     vec3 unit_direction = unit_vector(r.direction());
     // y element is in (-1,1)
@@ -14,6 +31,7 @@ color ray_color(const ray& r)
     // blended value = (1-a) * start value + a * end value
     return (1.0f-a)*color(1.0f,1.0f,1.0f) + a*color(0.5f,0.7f,1.0f);
 }
+
 
 int main()
 {
@@ -34,7 +52,9 @@ int main()
     // so when we direclty use that value, 
     // this might not match with actual integer image size values.
     // (Image height is clamped to have 1 at least and rounded integer value.)
-    auto viewport_width = viewport_height * static_cast<double>(image_width / image_height);
+    auto viewport_width = viewport_height * static_cast<double>(image_width) / image_height;
+    // static_cast<double>(image_width / image_height); is error, because
+    // it divids in int type, rounding down the decimal points, and convert to double.
     auto camera_center = point3(0,0,0);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
